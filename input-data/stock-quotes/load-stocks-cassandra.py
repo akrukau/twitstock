f5e9cde190ede2b0f9ea963ca0c60282ee52ef8e
Ubuntu <ubuntu@ip-172-31-0-105.us-west-2.compute.internal>
2016-02-02 01:39:54 +0000

    Refactored the code for loading stock quotes into Cassandra

--- input-data/stock-quotes/load-stocks-cassandra.py
+++ input-data/stock-quotes/load-stocks-cassandra.py
@@ -10,50 +10,42 @@ import json
 import sys
 import os
 
-def load_quote(line, session):
-    session.execute("INSERT INTO stocks JSON\'" + line + "\'") 
-
 # Read stock quotes
 configuration = SparkConf().setAppName("StocksData")
 spark_context = SparkContext(conf=configuration)
-#path = "./04-april-2013-stocks.json"
-path = "./sample.json"
-quotes_rdd = spark_context.textFile(path)
+path = "./04-april-2013-stocks.json"
+#path = "./sample.json"
 from pyspark.sql import SQLContext
 sqlContext = SQLContext(spark_context)
 df = sqlContext.jsonFile("./sample.json")
 df.show()
 
 # Write to Cassandra
-from cqlengine import columns
-from cqlengine.models import Model
-from cqlengine import connection
-from cqlengine.management import sync_table
+def aggToCassandraPart(agg):
+    from cassandra.cluster import Cluster
+    if agg:
+        cluster = Cluster(['52.88.73.44', '52.34.140.102', '52.34.147.146', '52.88.87.17'])
+        session = cluster.connect('tweet_keyspace')
 
-class stocks(Model):
-    ticker = columns.Text(primary_key=True)
-    #time = columns.DateTime(primary_key=True)
-    time = columns.Text(primary_key=True)
-    #type = columns.Text
-    price =  columns.Float
-    volume = columns.Integer
-    #ask_price =  columns.Float
-    #ask_volume = columns.Integer
-    #bid_price =  columns.Float
-    #bid_volume = columns.Integer
+        for quote in agg:
+            print "Quote",quote[0],"type",type(quote)
+            statement = "INSERT INTO stocks (ask_price, ask_volume, bid_price, bid_volume,"+ \
+                "price, ticker, time, type, volume)"+ \
+                "VALUES (%s, %s, %s, %s, %s, '%s', '%s', '%s', %s)" % quote
+                #"VALUES (%s, %s, %s, %s, %s, '%s', '%s', '%s', %s)" % (quote[0], quote[1], quote[2], \
+                # quote[3], quote[4], quote[5], quote[6], quote[7], quote[8])
+            print statement
+            session.execute(statement)
+            #session.execute('INSERT INTO stocks (ask_price, ask_volume, bid_price, bid_volume,'+ \
+                #     'price, ticker, time, type, volume)'+ \
+                #'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)' % (quote[0], quote[1], quote[2], \
+                #quote[3], quote[4], quote[5], quote[6], quote[7], quote[8]) )
+                #(quote['ask_price'], \
+                #quote['ask_volume'], quote['bid_price'], quote['bid_volume'], quote['price'], \
+                #quote['ticker'], quote['time'], quote['type'], quote['volume']) )
+        
+        session.shutdown()
+        cluster.shutdown()
 
-connection.setup(['52.88.73.44', '52.34.140.102', '52.34.147.146', '52.88.87.17'], "tweet_keyspace")
-sync_table(stocks)
-
-for quote in df:
-    try:
-        stocks.create(ticker = quote['ticker'], price = quote['price'], volume = quote['volume'])
-        #stocks.create(ticker = quote['ticker'], time = quote['time'], type = quote['type'], \
-        #    price = quote['price'], volume = quote['volume'], ask_price = quote['ask_price'], \
-        #    ask_volume = quote['ask_volume'], bid_price = quote['bid_price'], \
-        #    bid_volume = quote['bid_volume'])
-    except Exception as exception:
-        print "Exception is:", exception
-        #, "quote",quote
-
+df.foreachPartition(aggToCassandraPart)
 
